83456657851e5b96c880cb47201cfa0f0a717147
Anijan Nirmalan <anijan.nirmalan19@imperial.ac.uk>
2021-06-06 16:53:55 +0100

    cost imrovement

--- GraphTheory/tspt.py
+++ GraphTheory/tspt.py
@@ -5,19 +5,21 @@ from queue import PriorityQueue
 class Node:
 
     def __init__(self, path, matrix_reduced, cost, vertex, level):
-        self.path = path
-        self.path = []
+        self.path = path  # order of vertices
         self.matrix = matrix_reduced
         self.cost = cost
         self.vertex = vertex
         self.level = level
 
 
-def newnode(matrix_parent, level, i, j):
-
-    node = Node([], matrix_parent, calculatecost(matrix_parent), j, level)
-    node.path.append(j)
-
+def newnode( matrix_parent, level, i, j, prev_node=None):  # prev_node node class object of i 
+    if prev_node:
+        path = prev_node.path.append(j)
+    else:
+        path = [0]
+ 
+    node = Node(path, matrix_parent, calculatecost(matrix_parent), j, level)
+    
     for k in range(len(matrix_parent) - 1):
         if level != 0:
             node.matrix[i][k] = float('inf')
@@ -40,11 +42,12 @@ def rowreduction(matrix):
 def columnreduction(matrix):
 
     columnmin = np.min(matrix, axis=0)
+    print(columnmin)
 
     for j in range(len(matrix)):
         for i in range(len(matrix)):
-            if columnmin[i] != float('inf'):
-                matrix[i][j] = matrix[i][j] - columnmin[i]
+            if columnmin[j] != float('inf'): #i to j 
+                matrix[i][j] = matrix[i][j] - columnmin[j] # i to j
     return matrix
 
 
@@ -59,23 +62,19 @@ def calculatecost(matrix):
     rowmin = np.min(matrix, axis=1)
     matrix = rowreduction(matrix)
     columnmin = np.min(matrix, axis=0)
-
-    for num in rowmin:
-        if num != float('inf'):
-            cost += num
-
-    for num in columnmin:
-        if num != float('inf'):
-            cost += num
+    for num1, num2 in zip(rowmin, columnmin):
+        if num1 != float('inf'):
+            cost += num1
+        if num2 != float('inf'):
+            cost += num2
 
     return cost
 
 
 def tsp(matrix):
     pq = PriorityQueue()
-    for i in range(len(matrix)):
-        matrix[i][i] = float('inf')
-
+    matrix = matrix + np.diag([float('inf')]*len(matrix)) # mat change
+ 
     root = newnode(matrix, 0, -1, 0)
     root.matrix = reducedmatrix(root.matrix)
 
@@ -87,8 +86,7 @@ def tsp(matrix):
             pq.get()
         minnode = pq.queue[0]
         q = PriorityQueue()
-        q.put(minnode)
-        pq = q
+        q.put(minnode) # q kept
 
         if minnode[1].level == len(matrix) - 1:
             return minnode[1].path.append(0), minnode[1].cost
@@ -98,5 +96,4 @@ def tsp(matrix):
                 child = newnode(minnode[1].matrix, minnode[1].level + 1, minnode[1].vertex, j)
                 child.cost = minnode[1].cost + minnode[1].matrix[minnode[1].vertex][j] + calculatecost(child.matrix)
                 child.matrix = reducedmatrix(child.matrix)
-                pq.put((child.cost, child))
-
+                q.put((child.cost, child)) # add to q
