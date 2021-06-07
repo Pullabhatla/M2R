cdfda1a0f5c764a25f88bd1cd36502d6b78d7c40
Anijan Nirmalan <anijan.nirmalan19@imperial.ac.uk>
2021-06-07 14:26:22 +0100

    changed coordinate data to importable adjacency matrix

--- distance_data/__init__.py
+++ distance_data/__init__.py
@@ -1,2 +1,5 @@
 """Data import."""
 from .data import grid04, ha30, kn57, uk12, wg22  # noqa f401
+from .data import djibouti, usa, wsahara # noqa f401
+from .data import coord_to_matrix # noqa f401
+from .data import djibouti_adjacencymatrix, usa_adjacencymatrix, ws_adjacencymatrix # noqa f401
