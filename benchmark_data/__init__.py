aa9f537b34de8eef05b82df9c934b9484bc1cece
Anijan Nirmalan <anijan.nirmalan19@imperial.ac.uk>
2021-06-10 17:01:56 +0100

    benchmarkdata

--- benchmark_data/__init__.py
+++ benchmark_data/__init__.py
@@ -1,3 +1,4 @@
+"""Data package for benchmarking."""
 from .data import (kn_57, uk_12, sgb_128, djibouti_adjacencymatrix,  # noqa f401
                    ws_adjacencymatrix, vlsi_adjacencymatrix,
                    a280_adjacencymatrix)
