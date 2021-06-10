3c752e45c82c121597ce8bd62a692fae3f7fe036
Anijan Nirmalan <anijan.nirmalan19@imperial.ac.uk>
2021-06-10 16:59:08 +0100

    data package

---
+++ data/__init__.py
@@ -0,0 +1,4 @@
+from .data import (kn_57, uk_12, sgb_128, djibouti_adjacencymatrix,  # noqa f401
+                   ws_adjacencymatrix, vlsi_adjacencymatrix,
+                   a280_adjacencymatrix)
+from .data import coord_to_matrix # noqa f401
