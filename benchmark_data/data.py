047edc230ee42baa8324a71dc760b55a047edab4
Siddharth Pullabhatla <sp1319@ic.ac.uk>
2021-06-12 11:05:08 +0100

    data

--- benchmark_data/data.py
+++ benchmark_data/data.py
@@ -9,6 +9,7 @@ dj38 : 6656
 kn57 : 
 sgb128 :
 xqf131 : 564
+qa194 : 9352
 a280 : 2579
 uy734 : 79114
 zi929 : 95345
@@ -17,6 +18,7 @@ import pandas as pd
 import numpy as np
 import os
 from scipy.spatial.distance import euclidean
+from Req import Map
 
 
 def read_data(filename):
@@ -39,31 +41,42 @@ def coord_to_matrix(x, y, dist_function=None):
     return adj
 
 
-# adj matrix:
-kn57 = read_data('kn57_dist.csv')
-uk12 = read_data('uk12_dist.csv')
-sgb128 = read_data('sgb128_dist.csv')
-# the numpy array version
-kn_57 = kn57.to_numpy()
-uk_12 = uk12.to_numpy()
-sgb_128 = sgb128.to_numpy()
-# coord data:
-dj38 = read_data('dj38.csv')
-wi29 = read_data('wi29.csv')
-xqf131 = read_data('xqf131.csv')
-a280 = read_data('a280.csv')
-# can add more csv files in like this
+def gen_data(filename, dist_func=None):
+    """Read TSP data from a text file and generate an adjacency matrix."""
+    input = open(os.path.join(
+        os.path.dirname(__file__), "..", "benchmark_data", filename))
+    n = int(input.readline().strip().split()[1])
 
-dj_38 = dj38.to_numpy()
-wi_29 = wi29.to_numpy()
-xqf_131 = xqf131.to_numpy()
-a_280 = a280.to_numpy()
-# need to input x and y coord the right way
-# djabouti data file is arrange yx
-djibouti_adjacencymatrix = coord_to_matrix(dj_38[:, 1], dj_38[:, 0])
-# wsahara data dile is arranged yx
-ws_adjacencymatrix = coord_to_matrix(wi_29[:, 1], wi_29[:, 0])
-# vlsi data file is arranged yx
-vlsi_adjacencymatrix = coord_to_matrix(xqf_131[:, 1], xqf_131[:, 0])
-# a280 data file is arranged yx
-a280_adjacencymatrix = coord_to_matrix(a_280[:, 1], a_280[:, 0])
+    points = []
+
+    for i in range(n):
+        entry = input.readline().strip().split()
+        point = (float(entry[2]), float(entry[1]))
+        points.append(point)
+    if dist_func is None:
+        return Map(points).D
+    else:
+        return Map(points, dist_func).D
+
+
+# matrix data to adj:
+kn57 = read_data('kn57_dist.csv').to_numpy()
+uk12 = read_data('uk12_dist.csv').to_numpy()
+sgb128 = read_data('sgb128_dist.csv').to_numpy()
+
+# coord data to adj:
+dj38 = read_data('dj38.csv').to_numpy()
+dj38 = coord_to_matrix(dj38[:, 0], dj38[:, 1])
+
+wi29 = read_data('wi29.csv').to_numpy()
+wi29 = coord_to_matrix(wi29[:, 0], wi29[:, 1])
+
+xqf131 = read_data('xqf131.csv').to_numpy()
+xqf131 = coord_to_matrix(xqf131[:, 0], xqf131[:, 1])
+
+a280 = read_data('a280.csv').to_numpy()
+a280 = coord_to_matrix(a280[:, 0], a280[:, 1])
+
+uy734 = gen_data('uy734.txt')
+
+zi929 = gen_data('zi929.txt')
